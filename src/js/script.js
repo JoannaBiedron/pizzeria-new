f5034ac6aebf0d5f01a911626a2e1038c65139ab
Joanna Biedroń <joanna.biedron@gmail.com>
2020-02-25 20:59:53 +0100

    add js code / class CartProduct +constructor + getElements

--- src/js/script.js
+++ src/js/script.js
@@ -368,6 +368,36 @@
     }
   }
 
+  class CartProduct{
+    constructor(menuProduct, element){
+      const thisCartProduct = this;
+
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.price  = menuProduct.price;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.amount = menuProduct.amount;
+
+      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+
+      thisCartProduct.getElements(element);
+
+      console.log('thisCartProduct', thisCartProduct);
+    }
+
+    getElements(element){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom = {};
+
+      thisCartProduct.dom.wrapper = element;//wartość argumentu element
+      thisCartProduct.dom.amountWidget = thisCrtProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = thisCrtProduct.dom.wrapper.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = thisCrtProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = thisCrtProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+    }
+  }
+
   const app = {
     initMenu : function(){
       const thisApp = this;
