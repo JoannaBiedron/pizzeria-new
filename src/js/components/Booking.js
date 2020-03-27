0ef04bb1594cd35636d7b2d71c39cb3dfd385d01
Joanna Biedroń <joanna.biedron@gmail.com>
2020-03-27 14:37:24 +0100

    change js code

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,20 +1,23 @@
 import {select, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import {app} from '../app.js';
 
 class Booking{
-  constructor(bookingWidget){
+  constructor(){
     const thisBooking = this;
 
-    thisBooking.render(bookingWidget);
+    thisBooking.render();
     thisBooking.initWidgets();
   }
-  render(element){
+  render(){
     const thisBooking = this;
 
     const generatedHTML = templates.bookingWidget();
+    console.log('generatedHTML', generatedHTML);
 
     thisBooking.dom = {};
-    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper =
+    console.log('thisBooking.dom.wrapper', thisBooking.dom.wrpper);
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
 
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
