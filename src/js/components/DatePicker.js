c695adac13cf37cada4a1d69e7a55a881cfe4266
Joanna Biedroń <joanna.biedron@gmail.com>
2020-03-30 22:33:38 +0200

    Add js cone in new file DatePicker.js

---
+++ src/js/components/DatePicker.js
@@ -0,0 +1,56 @@
+import {select, settings} from '../settings.js';
+import {utils} from '../utils.js';
+import BaseWidget from ' ./BaseWidget.js';
+
+class DatePicker extends BaseWidget{
+  constructor(wrapper){
+    super(wrapper, utils.dateToStr(new Date()));
+
+    const thisWidget = this;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+
+    thisWidget.initPlugin();
+  }
+
+  initPlugin(){
+    const thisWidget = this;
+
+    thisWidget.minDate = new Date(thisWidget.value);
+    thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+
+    const options = {
+      defaultDate : thisWidget.minDate,
+      minDate : thisWidget.minDate,
+      maxDate : thisWidget.maxDate,
+      disable: [
+        function(date) {
+          return (date.getDay() === 1 );
+        }
+      ],
+      locale: {
+        'firstDayOfWeek': 1
+      },
+      onChange: function (selectedDates, dateToStr) {
+        thisWidget.value = dateToStr;
+      },
+    };
+
+    flatpickr(thisWidget.dom.input, options);
+  }
+
+
+  parseValue(element){
+    return  element;
+  }
+
+  isValid(){
+    return  true;
+  }
+
+  renderValue(){
+    console.log();
+  }
+}
+
+export default DatePicker;
